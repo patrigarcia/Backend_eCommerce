1fa265ddec86952801185871ea45145ca3512671
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-07-09 10:14:21 +0200

    Add Token Validation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,14 @@
-const { User } = require("../models/index.js");
+const { User, Token, Sequelize } = require("../models/index.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/config.json")["development"];
+const { Op } = Sequelize;
 
 const UserController = {
     async create(req, res) {
         try {
-            req.body.role = "user";
-            const user = await User.create(req.body);
+            const password = bcrypt.hashSync(req.body.password, 10);
+            const user = await User.create({ ...req.body, password: password });
             res.status(201).send({ message: "Usuario creado con éxito", user });
         } catch (err) {
             console.error(err);
@@ -38,6 +42,39 @@ const UserController = {
         } catch (err) {
             console.error(err);
             res.status(500).send({ message: "Ha habido un error al eliminar el usuario" });
+        }
+    },
+
+    async login(req, res) {
+        try {
+            const user = await User.findOne({ where: { email: req.body.email } });
+            if (!user) {
+                return res.status(400).send({ message: "Usuario o contraseña incorrecta" });
+            }
+            const isMatch = bcrypt.compareSync(req.body.password, user.password);
+            if (!isMatch) {
+                return res.status(400).send({ message: "Usuario o contraseña incorrecta" });
+            }
+            const token = jwt.sign({ id: user.id }, jwt_secret);
+            Token.create({ token, UserId: user.id });
+            res.send({ message: "Bienvenid@ " + user.name, user, token });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({ message: "Ha habido un error autenticando al usuario" });
+        }
+    },
+
+    async logout(req, res) {
+        try {
+            await Token.destroy({
+                where: {
+                    [Op.and]: [{ UserId: req.user.id }, { token: req.headers.authorization }],
+                },
+            });
+            res.send({ message: "Desconectado con éxito" });
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ message: "Hubo un problema al tratar de desconectarte" });
         }
     },
 };
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const UserController = require("../controllers/UserController"); //Acá se importa el user controller
+const UserController = require("../controllers/UserController"); 
 
 router.post("/", UserController.create);
 router.get("/", UserController.getAll);
-router.delete("/:mail", UserController.delete);
 router.put("/:name", UserController.update);
+router.post("/login", UserController.login);
+router.delete("/:mail", UserController.delete);
 
 module.exports = router;
